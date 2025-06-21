9d7de5753c620e4f049a0e253988f93b60483773
RealWarm <wjdgns8946@naver.com>
2025-06-21 18:29:06 +0900

    배치 기본 테스트

---
+++ src/main/java/com/hoon/hellospringbatch/job/HelloJobConfig.java
@@ -0,0 +1,11 @@
+package com.hoon.hellospringbatch.job;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfig {
+
+}
